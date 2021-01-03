f3619a5b08fb3a27d650f7e9690debb2126a4443
gracedotdotdot <gracelin0827@Gmail.com>
2021-01-03 19:51:41 +0800

    0103_avg_episode

--- PJM_With_RE_avg_episode.m
+++ PJM_With_RE_avg_episode.m
@@ -10,16 +10,16 @@ Rbar= -1;
 mybeta=0.1;
 
 %% RL state initialization
-pg=60; pd=62;
+pg=60; pd=70;
 So=[pg; pd; 0];
 n_train = 5740;
 org_state_array=zeros(n_train,3);
 final_state_energy = zeros(num_epi,1);
 energy = zeros(n_train,3);
-reward=zeros(n_train);
-final_reward = (n_train);
+reward=zeros(n_train,1);
+final_reward = zeros(n_train,1);
 A_array= zeros(n_train,1);
-final_price = zeros(n_train);
+final_price = zeros(n_train,1);
 mu = zeros(n_train,1);
 
 %% Tile coding initialization
@@ -27,9 +27,7 @@ N=8; % num of tilings
 p=[20 20 20]; % partition
 numTilesInTiling=prod(p);
 
-w= rand(N * prod(p),1);
-theta_mu= rand(N * prod(p),1);
-theta_sig = rand(N * prod(p),1);
+
 
 %% Read in Renewable Energy sources from 10/11 -11/8 PJM
 solar_file='../data/5_min_solar_gen.csv'; 
@@ -40,42 +38,45 @@ wind_data_daily = csvread(wind_file,75982,2,[75982 2 81739 2]);
 
 
 %cat 15 times of complete data section in original file to wind_data
-for i=1:15
-    wind_data = cat(1,wind_data,wind_data_daily);
-end
-wind_data_final = zeros(1,ceil(length(wind_data)/20));
-
-% %Since solar power reads every 5 minutes, we transform wind to 5 mins data
-%The original file is the newest data on the top, but we want to read the 
-%data in the series of time, we read from the bottom
-for i=1:ceil(length(wind_data)/20)
-    for j=1:20
-        wind_data_final(i) = wind_data_final(i)+wind_data(end - (i+j-2));
-    end
-    i= i+20;
-end
+% for i=1:15
+%     wind_data = cat(1,wind_data,wind_data_daily);
+% end
+% wind_data_final = zeros(1,ceil(length(wind_data)/20));
+% 
+% % %Since solar power reads every 5 minutes, we transform wind to 5 mins data
+% %The original file is the newest data on the top, but we want to read the 
+% %data in the series of time, we read from the bottom
+% for i=1:ceil(length(wind_data)/20)
+%     for j=1:20
+%         wind_data_final(i) = wind_data_final(i)+wind_data(end - (i+j-2));
+%     end
+%     i= i+20;
+% end
 
 %% Read every 15 sec
-% wind_data_final = zeros(1,n_train);
-% solar_data_final = zeros(1,n_train);
-% for i=1:n_train
-%     wind_data_final(i) = wind_data(end-i+1);
-% end
-% for i=1:ceil(n_train/20)
-%     for j = 1:20
-%         solar_data_final(i) = solar_data(end-(i+j-2));
-%     end
-%     i=i+20;
-% end
+wind_data_final = zeros(1,n_train);
+solar_data_final = zeros(1,n_train);
+for i=1:n_train
+    wind_data_final(i) = wind_data(end-i+1);
+end
+for i=1:ceil(n_train/20)
+    for j = 1:20
+        solar_data_final(i) = solar_data(end-(i+j-2));
+    end
+    i=i+20;
+end
     
 omg = 0.001*[solar_data(1:n_train)+wind_data(1:n_train)];
 
 %% main
 for epi=1:num_epi
-   S=So; 
-   i=1;
-   reach_goal_times=0;
-   org_state_array=zeros(n_train,3);
+    S=So; 
+    i=1;
+    reach_goal_times=0;
+    org_state_array=zeros(n_train,3);
+    w= rand(N * prod(p),1);
+    theta_mu= rand(N * prod(p),1);
+    theta_sig = rand(N * prod(p),1);
    fprintf("==================Reach goal ! next epi = %d==============================", epi);
    while i<n_train
         feaVec_S=featureState(S,N,p);
@@ -129,31 +130,30 @@ figure()
 subplot(2,1,1);
 t=1:n_train;
 %final_
-plot(t,reward);
+plot(t,final_reward);
 title('reward');
 subplot(2,1,2);
 t=1:length(A_array);
-plot(t,A_array);
+plot(t,final_price);
 title('price');
 xlabel('time(5 min)');
 
 figure()
 t=1:length(energy);
-plot(t, energy(:,1),'-o','DisplayName', 'Non-RES power supply');
+plot(t, energy(:,1),'-o','DisplayName', 'Non-RES power supply','LineWidth', 2);
 hold on;
-plot(t, energy(:,2),'--','DisplayName', 'Power demand');
+plot(t, energy(:,2),'--','DisplayName', 'Power demand','LineWidth', 2);
 hold on;
-plot(t, energy(:,3), ':', 'DisplayName', 'Energy storage');
+plot(t, energy(:,3), ':', 'DisplayName', 'Energy storage','LineWidth', 2);
 hold on;
-plot(t, omg(1:length(energy)),'DisplayName', 'RES power supply');
+plot(t, omg(1:length(energy)),'DisplayName', 'RES power supply','LineWidth', 2);
 hold on;
-plot(t, omg(1:length(energy))+energy(:,1), 'DisplayName', 'RES and non-RES power');
+plot(t, omg(1:length(energy))+energy(:,1), 'DisplayName', 'RES and non-RES power','LineWidth', 2);
 
-title('Reward = - ( abs(S(3)-1) )');
-%title('Reward = - ( 2*abs(S(1)+RE-S(2)) + abs(S(3)-1))');
-xlabel('time(5 min)');
-ylabel('power(GW) and energy(0.083GWh)');
-%legend({'power supply','power demand', 'energy storage', 'RES power',  'RES power + power supply'},'Location','northeast')
+xlabel('time(15 min)');
+ylabel('power(GW) and energy(0.25GWh)');
+axis([0 5730 -10 70])
+legend({'power supply','power demand', 'energy storage', 'RES power',  'RES power + power supply'},'Location','northeast')
 
 %% action selection
 function A = action(mu,sig)
@@ -200,9 +200,9 @@ function[S_prime, R]= env(S,A,del_t, RE)
     %if S(3)<0
         %R= - ( 2*abs(S(1)+RE-S(2))+abs(S(3)-1)) ;
     %else
-        %R= - ( 2*abs(S(1)+RE-S(2)) + abs(S(3)-1)); 
+        R= - ( 2*abs(S(1)+RE-S(2)) + abs(S(3)-1)); 
     %end
-    R= - ( abs(S(3)-1)) ;
+    %ㄜR= - ( abs(S(3)-1)) ;
 
 
 end
